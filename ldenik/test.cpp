60d16bde67f247f3cfb2b4db28f4d2471d1e9f30
Vašek Potoček <vasek.potocek@post.cz>
2022-04-09 19:08:07 +0200

    C++: test against ID manipulation

--- ldenik/test.cpp
+++ ldenik/test.cpp
@@ -2,8 +2,20 @@
 #include "ids.h"
 
 int main() {
-  int succ = 0;
-  for(int i = 0; i < 100000; i++)
-    succ += check(generate()) ? 1 : 0;
-  std::cout << (int)succ << '\n';
+  {
+    int succ = 0;
+    for(int i = 0; i < 100000; i++)
+      succ += check(generate()) ? 1 : 0;
+    std::cout << (int)succ << '\n';
+  }
+
+  {
+    int succ = 0;
+    for(int i = 0; i < 100000; i++) {
+      auto str = generate();
+      str[0] = 'a';
+      succ += check(str) ? 1 : 0;
+    }
+    std::cout << (int)succ << '\n';
+  }
 }
